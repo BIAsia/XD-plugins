813299e92936f1a2104b25a0cd710601cb4d1799
BIAsia <980492289@qq.com>
2021-02-02 18:45:38 +0800

    add shadowManager panel UI

--- ShadowManager/main.js
+++ ShadowManager/main.js
@@ -6,6 +6,7 @@
 
 
 let scenegraph = require("scenegraph"); 
+const { alert } = require("./lib/dialogs.js");
 var assets = require("assets");
 const {Color} = require("scenegraph"); 
 var shadowList = [];
@@ -23,28 +24,7 @@ function rectangleHandlerFunction(selection) {
 
 	// for test
 	if (JSON.stringify(shadowList[0]) == JSON.stringify(shadowList[1])) console.log("ok");
-	//showAlert(newColorNum, renameColorNum);
-	
-	/*
-	if (selection.hasArtboards){
-		// 选中的是画板
-		var artboardnames = [];
-		select.forEach((artboard) => {
-			artboard.children.foreach((child))
-		})
-
-	} else {
-		// 选中的是画板内or外的内容
-	}*/
-
-/*
-    const newElement = new Rectangle(); 
-    newElement.width = 100;
-    newElement.height = 50;
-    newElement.fill = new Color("Purple");
-
-    selection.insertionParent.addChild(newElement);
-    newElement.moveInParentCoordinates(100, 100);*/
+	showAlert(newColorNum, renameColorNum);
 
 }
 
@@ -94,7 +74,7 @@ function addColorToAssets(shadowcolor){
 
 // 添加阴影成功提示
 async function showAlert(newColorNum, renameColorNum){
-    await alert(newColorNum + "shadow colors has been added to Assets", //[1]
+    await alert(newColorNum + " shadow colors has been added to Assets", //[1]
     "Including " + renameColorNum + " repeated shadow colors",
     "Go to Assets Panel to see your colors :D"); //[2]
 }
@@ -119,7 +99,98 @@ function outputParentDetails(child){
 	}
 }
 
+let panel;
+function create() {
+	// [1]
+	const html = `
+	<style>
+		.break {
+			flex-wrap: wrap;
+		}
+		label.row > span {
+			color: #8E8E8E;
+			width: 20px;
+			text-align: right;
+			font-size: 9px;
+		}
+		label.row input {
+			flex: 1 1 auto;
+		}
+		form {
+			width:90%;
+			padding: 0px;
+		}
+		.show {
+			display: block;
+		}
+		.hide {
+			display: none;
+		}
+	</style>
+	
+	<form id="main">
+		<div class="row break">
+			<label class="row">
+				<span>↕︎</span>
+				<input type="number" uxp-quiet="true" id="txtV" value="10" placeholder="Height" />
+			</label>
+			<label class="row">
+				<span>↔︎</span>
+				<input type="number" uxp-quiet="true" id="txtH" value="10" placeholder="Width" />
+			</label>
+		</div>
+		<footer><button id="ok" type="submit" uxp-variant="cta">Apply</button></footer>
+	</form>
+	
+	<p id="warning">This plugin requires you to select a rectangle in the document. Please select a rectangle.</p>
+	`;
+	
+	  function increaseRectangleSize() { // [2]
+		const { editDocument } = require("application"); // [3]
+		const height = Number(document.querySelector("#txtV").value); // [4]
+		const width = Number(document.querySelector("#txtH").value); // [5]
+	
+		// [6]
+		editDocument({ editLabel: "Increase rectangle size" }, function(selection) {
+		  const selectedRectangle = selection.items[0]; // [7]
+		  selectedRectangle.width += width; // [8]
+		  selectedRectangle.height += height;
+		});
+	  }
+	
+	  panel = document.createElement("div"); // [9]
+	  panel.innerHTML = html; // [10]
+	  panel.querySelector("form").addEventListener("submit", increaseRectangleSize); // [11]
+	
+	  return panel; // [12]
+}
+
+function show(event) {
+	if (!panel) event.node.appendChild(create()); // [2]
+}
+
+function update(selection) {
+	const { Rectangle } = require("scenegraph"); // [2]
+
+	const form = document.querySelector("form"); // [3]
+	const warning = document.querySelector("#warning"); // [4]
+
+	if (!selection || !(selection.items[0] instanceof Rectangle)) { // [5]
+		form.className = "hide";
+		warning.className = "show";
+	} else {
+		form.className = "show";
+		warning.className = "hide";
+	}
+}
+
 module.exports = {
+	panels: {
+		showShadows: {
+			show,
+			update
+		}
+	},
     commands: {
         createRectangle: rectangleHandlerFunction
     }
